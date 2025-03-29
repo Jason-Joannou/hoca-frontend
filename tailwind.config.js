2646c10b36a5034decaa8032f3596342c97486bf
Jason Joannou <jjoannou1099@gmail.com>
2025-03-29 11:12:16 +0200

    Added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Make sure this includes your files
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
