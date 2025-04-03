342fa6eefbe292c97f7e9a31dcbff8c040ec4edc
Jason Joannou <jjoannou1099@gmail.com>
2025-04-03 15:48:52 +0200

    Updates to configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Make sure this includes your files
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Make sure this includes your files
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
