f85840bc2f2d5cc703175b43a2e4bbf897215a54
Jason Joannou <jjoannou1099@gmail.com>
2025-04-03 15:49:15 +0200

    Created main workshop page

---
+++ src/components/dashboard/workshops/main-workshop.tsx
@@ -0,0 +1,5 @@
+import ComingSoon from "@/components/utils/coming-soon";
+
+export default function MainEventPage({}) {
+  return <ComingSoon message="Workshops are currently under construction!" />;
+}
