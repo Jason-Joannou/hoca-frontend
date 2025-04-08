eae9010dc0bbe8fc62bf40b84e3357a3e90bc0c8
Jason Joannou <jjoannou1099@gmail.com>
2025-04-08 23:38:29 +0200

    New styling for main dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -45,11 +45,15 @@ export default function Page() {
             </Breadcrumb>
           </div>
         </header>
-        <div>
-          {selectedPage === "Events" && <MainEventsPage />}
-          {selectedPage === "Workshops" && <MainWorkshopPage />}
-          {selectedPage === "Talks" && <p>Talks Content</p>}
-          {selectedPage === "Home" && <p>Dashboard Home</p>}
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              {selectedPage === "Events" && <MainEventsPage />}
+              {selectedPage === "Workshops" && <MainWorkshopPage />}
+              {selectedPage === "Talks" && <p>Talks Content</p>}
+              {selectedPage === "Home" && <p>Dashboard Home</p>}
+            </div>
+          </div>
         </div>
       </SidebarInset>
     </SidebarProvider>
